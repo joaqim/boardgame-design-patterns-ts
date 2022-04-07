613b67c318575a140d43a8ae9568e57e9253c047
Joaqim Planstedt <git@joaqim.xyz>
2022-04-07 22:08:33 +0200

    add missing default export from graph/

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -1,7 +1,4 @@
-import Graph from "./graph";
-
 export * from "./graph";
+export { default as Graph } from "./graph";
 export * from "./graph-meta-data";
 export * from "./graph-utils";
-
-export default Graph;

7e482daa2794b08498dde91adb19f52f2497abb6
Joaqim Planstedt <git@joaqim.xyz>
2022-04-21 13:41:28 +0200

    initial working Adjacency Matrix

--- src/containers/adjacency-matrix.ts
+++ src/containers/adjacency-matrix.ts
@@ -1,13 +1,42 @@
+import type * as graph from "../graph";
 import type { FixedArray } from "./fixed-array";
 
 export type AdjacencyMatrix<TSize extends number> = FixedArray<
   TSize,
   FixedArray<TSize, 0 | 1>
->;
+> &
+  number[][];
 
-export const AdjacencyMatrixIdentity = <TSize extends number>(
+const AdjacencyMatrixIdentity = <TLength extends number>(
   size: number
-): AdjacencyMatrix<TSize> =>
-  Array.from({ length: size }).fill(
+): AdjacencyMatrix<TLength> =>
+  Array.from({ length: size }, () =>
     Array.from({ length: size }).fill(0)
-  ) as AdjacencyMatrix<TSize>;
+  ) as AdjacencyMatrix<TLength>;
+
+export interface AdjacencyMatrixMetaData<
+  TLength extends number,
+  TNode extends number = graph.Node<TLength>,
+  TEdge extends number[] = graph.Edge<TNode>
+> {
+  length: TLength;
+  edges?: TEdge[];
+}
+
+export const createAdjacencyMatrix = <
+  TLength extends number,
+  TNode extends number = graph.Node<TLength>
+>(
+  data: AdjacencyMatrixMetaData<TLength, TNode>
+): AdjacencyMatrix<TLength> => {
+  const matrix = AdjacencyMatrixIdentity<TLength>(data.length);
+
+  data.edges?.forEach((edge) => {
+    const start = edge[0];
+    const end = edge[1];
+    matrix[start][end] = 1;
+    matrix[end][start] = 1;
+  });
+
+  return matrix;
+};

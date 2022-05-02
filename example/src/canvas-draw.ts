2b5060f0aecd20bd9a500c4b05ca71fd7835b528
Joaqim Planstedt <git@joaqim.xyz>
2022-05-02 14:20:12 +0200

    minor syntax fixes and additions

--- example/src/canvas-draw.ts
+++ example/src/canvas-draw.ts
@@ -8,8 +8,10 @@ export function CanvasArrow(
 ): void {
   // Length of head in pixels
   const headlen = 24;
+  // Difference between points of vector
   const dx = tox - fromx;
   const dy = toy - fromy;
+  // Angle of vector
   const angle = Math.atan2(dy, dx);
 
   context.moveTo(fromx, fromy);
@@ -32,18 +34,22 @@ export function CanvasHalfArrow(
   tox: number,
   toy: number
 ): void {
-  // Length of head in pixels
+  // Length of head desired in pixels
   const headlen = 24;
+  // Difference between points of vector
   const dx = tox - fromx;
   const dy = toy - fromy;
 
-  // Shorten arrow length by half
+  // Angle of vector
   const angle = Math.atan2(dy, dx);
+  // Pythagora's Theorem to calculate length of vector
   const length = Math.sqrt(dx ** 2 + dy ** 2);
-  fromx -= (length / 2) * Math.cos(angle + Math.PI);
-  fromy -= (length / 2) * Math.sin(angle + Math.PI);
 
-  context.moveTo(fromx, fromy);
+  // Start arrow from half the length
+  context.moveTo(
+    fromx - (length / 2) * Math.cos(angle + Math.PI),
+    fromy - (length / 2) * Math.sin(angle + Math.PI)
+  );
   context.lineTo(tox, toy);
   context.moveTo(tox, toy);
   context.lineTo(
--- example/src/main.ts
+++ example/src/main.ts
@@ -5,8 +5,11 @@ import TalismanGraphConfig from "../graphs/talisman-graph-config";
 import { CanvasHalfArrow } from "./canvas-draw";
 import { TalismanNodeLayoutRelativePosition } from "./talisman-node-layout";
 
-type GraphNode = [x: number, y: number];
-type GraphEdge = [a: number, b: number];
+// Where number is pixel position of node
+type Position = [x: number, y: number];
+
+// Where number is index of Node
+type Edge = [a: number, b: number];
 
 class DrawingApp {
   private readonly canvas: HTMLCanvasElement;
@@ -94,14 +97,14 @@ class DrawingApp {
     this.drawGraph(this.graph);
   }
 
-  private drawNode(node: GraphNode, radius = 6): void {
+  private drawNode(position: Position, radius = 6): void {
     const context = this.context;
     context.beginPath();
     context.strokeStyle = "black";
     context.fillStyle = "blue";
     context.arc(
-      node[0] + radius,
-      node[1] + radius,
+      position[0] + radius,
+      position[1] + radius,
       radius,
       0,
       Math.PI * 2,
@@ -112,7 +115,7 @@ class DrawingApp {
   }
 
   private drawEdge(
-    edge: GraphEdge,
+    edge: Edge,
     bidirectional = true,
     conditional = false
   ): void {
@@ -144,9 +147,9 @@ class DrawingApp {
     context.stroke();
   }
 
-  private drawGraph(graph: Graph<49>): void {
+  private drawGraph<T extends number>(graph: Graph<T>): void {
     graph.forEachEdge(
-      (edge: GraphEdge, bidirectional: boolean, conditional: boolean): void => {
+      (edge: Edge, bidirectional: boolean, conditional: boolean): void => {
         this.drawEdge(edge, bidirectional, conditional);
       },
       this.graphConditions

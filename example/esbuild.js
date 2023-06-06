838a9e56000746977212eb626030639a6dd8946d
Joaqim Planstedt <git@joaqim.xyz>
2023-06-06 23:17:59 +0200

    Update build of sub-package: ./example

--- example/esbuild.js
+++ example/esbuild.js
@@ -7,20 +7,20 @@ const inlineImage = require("esbuild-plugin-inline-image");
 (async () => {
   await mkdir("./dist");
 
-  const script = await esbuild.build({
-    entryPoints: ["src/main.ts"],
-    bundle: true,
-    minify: true,
-    format: "esm",
-    target: ["esnext"],
-    write: false,
-    outdir: "./dist",
-    plugins: [inlineImage()]
-  }).catch(() => process.exit(1));
+  const script = await esbuild
+    .build({
+      entryPoints: ["src/main.ts"],
+      bundle: true,
+      minify: true,
+      format: "esm",
+      target: ["esnext"],
+      write: true,
+      outdir: "./dist",
+      plugins: [inlineImage()],
+    })
+    .catch(() => process.exit(1));
 
-  await writeFile(script.outputFiles[0].path, script.outputFiles[0].contents)
-
-  const html = await readFile("src/index.html", "utf8");
+  const html = await readFile("public/index.html", "utf8");
 
   const minifyOptions = {
     collapseWhitespace: true,
@@ -35,10 +35,6 @@ const inlineImage = require("esbuild-plugin-inline-image");
 
   await writeFile(
     "dist/index.html",
-    `${await minify(
-      html,
-      minifyOptions
-    )}<script>${script.outputFiles[1].text}</script>`
+    `${await minify(html, minifyOptions)}<script type="module" src="./main.js"></script>`
   );
-
 })();
